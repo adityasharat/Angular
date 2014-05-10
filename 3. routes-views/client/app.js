7d3bad372c95481dc0383bad2335e038c114ff7d
Aditya Sharat <adityasharat@gmail.com>
2014-05-10 17:34:24 +0530

    Added header to switch between views
    A new directive <header>.
    This is will add the app header, with the navigation links

--- 3. routes-views/client/app.js
+++ 3. routes-views/client/app.js
@@ -100,13 +100,32 @@
         NotesService.fetch();
     });
 
+    NotesApp.directive('header', function () {
+        // Runs during compile
+        return {
+            name: 'header',
+            // priority: 1,
+            // terminal: true,
+            // scope: {}, // {} = isolate, true = child, false/undefined = no change
+            controller: function($scope, $element, $attrs, $transclude) {},
+            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+            restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+            // template: '',
+            templateUrl: 'templates/header.html',
+            replace: true,
+            // transclude: true,
+            // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+            //link: function($scope, iElm, iAttrs, controller) {}
+        };
+    });
+
     NotesApp.directive('notes', function () {
         // Runs during compile
         return {
             name: 'notes',
             // priority: 1,
             // terminal: true,
-            scope: {}, // {} = isolate, true = child, false/undefined = no change
+            //scope: {}, // {} = isolate, true = child, false/undefined = no change
             controller: function ($scope, $element, $attrs, $transclude, NotesService, $timeout) {
                 $scope.notes = NotesService.all();
                 NotesService.then(null, null, function (notes) {

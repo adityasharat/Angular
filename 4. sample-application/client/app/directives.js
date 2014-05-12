8bcfc485afd2be2c79ce93189c600a0873c5fe4c
Aditya Sharat <adityasharat@gmail.com>
2014-05-12 22:50:00 +0530

    improved styling. bug fixes.
    Improved the styling for events pages. fixed bug with header, now the
    tab should be selected according to the view.

--- 4. sample-application/client/app/directives.js
+++ 4. sample-application/client/app/directives.js
@@ -9,7 +9,11 @@
             // priority: 1,
             // terminal: true,
             // scope: {}, // {} = isolate, true = child, false/undefined = no change
-            //controller: function ($scope, $element, $attrs, $transclude) {},
+            controller: function ($scope, $element, $attrs, $transclude, $location) {
+                $scope.isActive = function (viewLocation) {
+                    return viewLocation === $location.path();
+                };
+            },
             // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
             restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
             // template: '',

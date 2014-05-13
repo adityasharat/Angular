22818206b8fc041390abad3f3cea086a320db8c6
Aditya Sharat <adityasharat@gmail.com>
2014-05-13 22:50:37 +0530

    Changed the edit event interface
    Added a edit icon on the top right.

--- 4. sample-application/client/app/directives.js
+++ 4. sample-application/client/app/directives.js
@@ -10,7 +10,7 @@
             // terminal: true,
             scope: {
                 view : '='
-            }, // {} = isolate, true = child, false/undefined = no change
+            },
             controller: function ($scope, $element, $attrs, $transclude, $location) {},
             // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
             restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
@@ -55,11 +55,7 @@
             // priority: 1,
             // terminal: true,
             // scope: {}, // {} = isolate, true = child, false/undefined = no change
-            controller: function ($rootScope, $scope, $location, $element, $attrs, $transclude) {
-                $scope.click = function () {
-                    $location.path('editevent/' + $scope.event.id);
-                };
-            },
+            // controller: function ($rootScope, $scope, $location, $element, $attrs, $transclude) {},
             require: '^events', // Array = multiple requires, ? = optional, ^ = check parent elements
             restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
             templateUrl: 'templates/event.html',

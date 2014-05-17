2f9aa15e018a86210fe7ba343c1989e69ae95ae4
Aditya Sharat <adityasharat@gmail.com>
2014-05-17 21:30:46 +0530

    Table view implemented.
    Awaiting v1.3 beta 8 for the fix in $compile for tables. issue #1459

--- 4. sample-application/client/app/directives.js
+++ 4. sample-application/client/app/directives.js
@@ -71,7 +71,7 @@
             // controller: function ($scope, $element, $attrs, $transclude, EventsService, $timeout) {},
             // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
             restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-            templateUrl: 'templates/events-board-view.html',
+            templateUrl: 'templates/events-table-view.html',
             replace: true,
             // transclude: true,
             // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
